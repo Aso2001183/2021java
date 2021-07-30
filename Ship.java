fba8189d0fa17f54dc76f682b08faf484da23e52
Aso2001183 <83046808+Aso2001183@users.noreply.github.com>
2021-07-30 16:38:24 +0900

    Add files via upload

---
+++ Ship.java
@@ -0,0 +1,14 @@
+package Senkan;
+
+
+public class Ship {
+     public static void main(String[] args) {
+        
+     {
+        System.out.println("******************");
+        System.out.println("   戦艦ゲーム    ");
+        System.out.println("******************");
+    }
+}
+    
+}
